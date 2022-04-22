6b46378eaf72366a729da40dfdd2fcf646df7ca2
hajer-nasr <nasrhajer1@gmail.com>
2022-04-22 01:51:28 +0100

    storageFile

--- Back/index.js
+++ Back/index.js
@@ -1,26 +1,25 @@
-'use strict';
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+"use strict";
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-const userRoutes = require('./routes/userRoutes');
-const fileRoutes = require('./routes/fileRoutes');
+const userRoutes = require("./routes/userRoutes");
+const fileRoutes = require("./routes/fileRoutes");
 
 const app = express();
 const port = 3000;
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/api',userRoutes.routes);
-app.use('/file',fileRoutes.routes);
+app.use("/api", userRoutes.routes);
+app.use("/file", fileRoutes.routes);
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-  })
-
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})  
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- Back/routes/fileRoutes.js
+++ Back/routes/fileRoutes.js
@@ -2,3 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(null, "dataset.json");
+  },
+});
