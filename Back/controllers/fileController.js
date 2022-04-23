dc8135c166eff638f8d135203ebf609b81ccf3d9
hajer-nasr <nasrhajer1@gmail.com>
2022-04-24 00:54:49 +0100

    moduleUpload

--- Back/controllers/fileController.js
+++ Back/controllers/fileController.js
@@ -1,23 +1,22 @@
 "use strict";
 const fs = require("fs");
 
-const upload = async(req, res) => { 
+const upload = async (req, res) => {
   //
-  console.log("hell");
+  console.log("hello");
   console.log(req.body.test1);
-  var txt =JSON.stringify(req.body);
+  var txt = JSON.stringify(req.body);
   console.log(txt);
-  fs.writeFile("uploads/data.json", txt, 'utf8', function (err) {
+  fs.writeFile("uploads/data.json", txt, "utf8", function (err) {
     if (err) {
-        console.log("An error occured while writing JSON Object to File.");
-        return console.log(err);
+      console.log("An error occured while writing JSON Object to File.");
+      return console.log(err);
     }
- 
-    console.log("JSON file has been saved.");
-    res.send({text : "loading json work well"});
-});
 
-}
+    console.log("JSON file has been saved.");
+    res.send({ text: "loading json work well" });
+  });
+};
 
 const download = async (req, res) => {};
 
