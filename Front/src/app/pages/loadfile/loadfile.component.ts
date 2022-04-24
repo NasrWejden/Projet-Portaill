e12654f0f2241ac0ae1d5ceb3be6a3119c0a5d67
NasrWejden <wejdnasr98@gmail.com>
2022-04-24 18:21:35 +0100

    Commit loadfile

--- Front/src/app/pages/loadfile/loadfile.component.ts
+++ Front/src/app/pages/loadfile/loadfile.component.ts
@@ -15,12 +15,42 @@ import { SpinnersAngularModule } from 'spinners-angular';
 })
 export class LoadfileComponent implements OnInit {
 
+  dataJson : any;
+  prediction : number[];
+  fs: any;
+  spinner = false ;
+  no_spinner = true ;
 
-  constructor() { }
+  constructor(private datasetservice : DatasetService,private http: HttpClient , private router:Router) { }
 
   ngOnInit(): void {
-
+    this.dataJson = [];
+    //this.datasetservice.getPrediction().subscribe (prediction => this.prediction = prediction);
   }
 
-  
+  fileUpload(event:any){
+    this.spinner = true ;
+    this.no_spinner = false ;
+    const selectedFile = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.readAsBinaryString(selectedFile);
+    fileReader.onload = (event) => {
+      let binarydata = event.target.result;
+      let workbook = XLSX.read(binarydata , {type : 'binary'});
+      workbook.SheetNames.forEach(sheet => {
+        const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet]);
+        this.dataJson = data; 
+        const body=JSON.stringify(data);
+        const headers = { 'content-type': 'application/json'} 
+        console.log(body);
+        this.datasetservice.loadjson(body).subscribe(msg => console.log(msg),error => console.log(error));
+        this.datasetservice.setDatajson(this.dataJson);
+        this.datasetservice.train();
+        this.router.navigate(['/datatable']);
+
+      })
+      
+    } 
+  }
+
 }
